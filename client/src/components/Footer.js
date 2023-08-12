27ef3e6af1465e21a09e5209ac837e21810bb61a
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-13 01:45:51 +0900

    feat: footer, menu 파일 설정

---
+++ client/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+
+    return (
+        <footer></footer>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Menu.js
@@ -0,0 +1,8 @@
+const Menu = () => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default Menu;
