f522b7735a98c875143b4c3d2224d083f52669d2
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-13 00:08:29 +0900

    feat: header jsx code ongoing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ const Header = () => {
 
     return (
         <header>
-
+            <a href>
+                <img src={require('../static/logo.png')} alt="로고" />
+            </a>
+            <div></div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
         </header>
     )
 }
