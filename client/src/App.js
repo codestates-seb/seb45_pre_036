0342438f2291cd7a28af6c8335153fc1ad39fb1f
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-13 01:48:19 +0900

    feat: 테스트로 App.js에 넣어둔 Header컴포넌트 주석처리

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import Header from './components/Header';
+// import Header from './components/Header';
 import './styles/App.css';
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
     </div>
     );
 }
