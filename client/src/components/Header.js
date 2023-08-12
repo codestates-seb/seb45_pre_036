4d38f5f3645519e00d8a963ad5ed74d5bb391835
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-13 00:18:36 +0900

    feat: header jsx ongoing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,22 @@
 const Header = () => {
-
-    return (
-        <header>
-            <a href>
-                <img src={require('../static/logo.png')} alt="로고" />
-            </a>
-            <div></div>
-            <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <a href>
+        <img src={require("../static/logo.png")} alt="stackoverflow logo" />
+      </a>
+      <div>
+        <div>profile</div>
+        <nav>
+          <ul>
+            <li><img src={require("../static/inbox.png")} alt="alarm-inbox" /></li>
+            <li><img src={require("../static/trophy.png")} alt="achievement" /></li>
+            <li><img src={require("../static/help.png")} alt="help" /></li>
+            <li><img src={require("../static/stack.png")} alt="stack-exchange" /></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
