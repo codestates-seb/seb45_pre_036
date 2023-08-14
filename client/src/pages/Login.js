49864bbe591939f41808c8715536f19f3af401dc
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-14 14:54:00 +0900

    feat:login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,18 @@
 const Login = () => {
 
     return (
-        <div>
-            <div></div>
-            <form>
-                <label>Email</label>
-                <input></input>
-                <label>Password</label>
-                <input></input>
-                <button>Log in</button>
-                <hr />
-                <button><img src alt/>Log in with Github</button>
+        <div className='login-form'>
+            <div className='login-form__logo'>Logo</div>
+            <form className='login-form__form' onSubmit>
+                <label className='login-form__label' htmlFor='email'>Email</label>
+                <input className='login-form__input' id='email' type='text' ></input>
+                <label className='login-form__label' htmlFor='password'>Password</label>
+                <input className='login-form__input' type='password' id='password'></input>
+                <button className='login-form__button' >Log in</button>
+                <hr className='login-form__divider' />
+                <button className='login-form__github-button' type='submit'><img className='login-form__github-logo' src alt/>Log in with Github</button>
             </form>
-            <p>Don't have an account? <span><a href>Sign up</a></span></p>
+            <p className='login-form__signup-text'>Don't have an account? <a href className="login-form__signup-link">Sign up</a></p>
         </div>
     )
 }
