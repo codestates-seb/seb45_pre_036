303441ec91959340de7796e9dfd2b247149a7fb8
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-14 16:08:30 +0900

    feat: login styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,47 @@
-const Login = () => {
+import '../styles/pages/Login.css';
 
-    return (
-        <div className='login-form'>
-            <div className='login-form__logo'>Logo</div>
-            <form className='login-form__form' onSubmit>
-                <label className='login-form__label' htmlFor='email'>Email</label>
-                <input className='login-form__input' id='email' type='text' ></input>
-                <label className='login-form__label' htmlFor='password'>Password</label>
-                <input className='login-form__input' type='password' id='password'></input>
-                <button className='login-form__button' >Log in</button>
-                <hr className='login-form__divider' />
-                <button className='login-form__github-button' type='submit'><img className='login-form__github-logo' src='' alt=''/>Log in with Github</button>
-            </form>
-            <p className='login-form__signup-text'>Don't have an account? <a href className="login-form__signup-link">Sign up</a></p>
-        </div>
-    )
-}
+const Login = () => {
+  return (
+    <container className="login-container">
+      <div className="login-form__logo">Logo</div>
+      <div className="login-form__container">
+        <form className="login-form__form" onSubmit>
+          <div className="login-form__item">
+            <label className="login-form__label" htmlFor="email">
+              Email
+            </label>
+            <input className="login-form__input" id="email" type="text"></input>
+          </div>
+          <div className="login-form__item">
+            <label className="login-form__label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="login-form__input"
+              type="password"
+              id="password"
+            ></input>
+          </div>
+          <div className="login-form__item">
+            <button className="login-form__button">Log in</button>
+          </div>
+          <hr className="login-form__divider" />
+          <div className="login-form__item">
+            <button className="login-form__github-button" type="submit">
+              <img className="login-form__github-logo" src="" alt="" />
+              Log in with Github
+            </button>
+          </div>
+        </form>
+      </div>
+      <p className="login-form__signup-text">
+        Don't have an account?{" "}
+        <a href className="login-form__signup-link">
+          Sign up
+        </a>
+      </p>
+    </container>
+  );
+};
 
 export default Login;
