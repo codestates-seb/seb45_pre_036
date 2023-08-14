22b850a19359525db1e0e56412a64a621f6bdf9a
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-14 14:18:45 +0900

    feat: Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,20 @@
+const Login = () => {
+
+    return (
+        <div>
+            <div></div>
+            <form>
+                <label>Email</label>
+                <input></input>
+                <label>Password</label>
+                <input></input>
+                <button>Log in</button>
+                <hr />
+                <button><img src alt/>Log in with Github</button>
+            </form>
+            <p>Don't have an account? <span><a href>Sign up</a></span></p>
+        </div>
+    )
+}
+
+export default Login;
