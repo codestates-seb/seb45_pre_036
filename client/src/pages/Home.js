89508ed890c2ca92a9fdb333f799047109780bf6
yaejun98 <shinyj0548@gmail.com>
2023-08-14 15:50:07 +0900

    feat: 홈 페이지 기본 frame

---
+++ client/src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Header from '../component/Header'
+import Footer from '../components/Footer'
+import Menu from '../components/Menu'
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+    <Header />
+      <div className='home__container'>
+        <div className='userInfo__container'>
+          <div className='userInfo__wrapper'>
+            <div className="userInfo__input">
+             <div className="userInfo__inputWrapper">
+                </div>
+              </div>
+          </div>
+        </div>
+        <section className='about'>
+          <p>About</p>
+          <div className='about__container'></div>
+        </section>
+        <section className='questions'>Questions
+          <p>Questions</p>
+          <div className='questions__container'></div>
+        </section>
+        <section className='answers'>Answers
+        <p>Answers</p>
+          <div className='answers_container'></div>
+        </section>
+        <Footer />
+      </div>
+      </div>
+  );
+}
+
+export default Home;
