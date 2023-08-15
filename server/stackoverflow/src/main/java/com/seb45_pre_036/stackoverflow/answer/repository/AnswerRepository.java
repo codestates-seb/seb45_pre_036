5e3e0a40735844a859960818bd2e5fbf4677f597
0KSY <uryhon7@gmail.com>
2023-08-15 13:31:28 +0900

    feat/member

--- server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seb45_pre_036.stackoverflow.answer.repository;
-
-import com.seb45_pre_036.stackoverflow.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seb45_pre_036.stackoverflow.comment.repository;
-
-import com.seb45_pre_036.stackoverflow.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
