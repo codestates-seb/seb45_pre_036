ae4cce3053324c8d18b3be6bd78ff4130af4f416
2-eun-ji <leeijnue@gmail.com>
2023-08-15 13:59:28 +0900

    feat: 댓글 CRUD 구현

--- server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/answer/mapper/AnswerMapper.java
+++
@@ -1,4 +0,0 @@
-package com.seb45_pre_036.stackoverflow.answer.mapper;
-
-public interface AnswerMapper {
-}
--- server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/question/entity/Question.java
+++
@@ -1,4 +0,0 @@
-package com.seb45_pre_036.stackoverflow.question.entity;
-
-public class Question {
-}
