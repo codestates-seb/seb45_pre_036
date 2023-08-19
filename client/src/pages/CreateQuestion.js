e54f5f221f01ce178b8391e907815d04f74c0ed8
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-19 19:05:20 +0900

    feat: createpost ongoing

--- client/src/pages/CreateQuestion.js
+++ client/src/pages/CreateQuestion.js
@@ -1,30 +1,66 @@
+import { useState } from "react";
 import TextEditor from "../components/TextEditor";
-import '../styles/pages/CreateQuestion.css';
+import "../styles/pages/CreateQuestion.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CreateQuestion = () => {
-  // title이랑 content로 body 보내기
-  // header로 accessToken 보내기
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const createPostHandler = async () => {
+    const url = "http://localhost:8080/questions/ask";
+    const headers = {
+      // Authorization: `Bearer ${accessToken}`, // 
+    };
+    const body = {
+      // memberId, // accessToken payload에 있음. 
+      title,
+      // content, // TextEditor에서 가져와야.
+    };
+
+    try {
+      const response = await axios.post(url, body, { headers });
+      console.log("Post created successfully:", response.data);
+      navigate('/');
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
 
   return (
-    <div class="ask-question">
-      <h1 class="ask-question__heading">Ask a public question</h1>
-      <div class="ask-question__container title">
-        <h2 class="ask-question__title">Title</h2>
-        <p class="ask-question__desc">
+    <div className="ask-question">
+      <h1 className="ask-question__heading">Ask a public question</h1>
+      <div className="ask-question__container title">
+        <h2 className="ask-question__title">Title</h2>
+        <p className="ask-question__desc">
           Be specific and imagine you're asking a question to another person.
         </p>
-        <input class="ask-question__title-input" placeholder="Enter a title of your question."></input>
-        <button class="ask-question__button next" type="button">Next</button>
+        <input
+          className="ask-question__title-input"
+          placeholder="Enter a title of your question."
+          value={title}
+          onChange={titleChangeHandler}
+        ></input>
+        <button class="ask-question__button next" type="button">
+          Next
+        </button>
       </div>
-      <div class="ask-question__container content">
-        <h2 class="ask-question__title">What are details of your problem?</h2>
-        <p class="ask-question__desc">
+      <div className="ask-question__container content">
+        <h2 className="ask-question__title">
+          What are details of your problem?
+        </h2>
+        <p className="ask-question__desc">
           Introduce the problem and expand on what you put in the title.
         </p>
-        <div class="ask-question__text-editor">
+        <div className="ask-question__text-editor">
           <TextEditor />
         </div>
-        <button class="ask-question__button create">Create</button>
+        <button className="ask-question__button create" onClick={createPostHandler}>Create</button>
       </div>
     </div>
   );
