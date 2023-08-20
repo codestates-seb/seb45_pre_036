bf63d5f0feddab82c4aa969ae669369027b327b2
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-21 03:39:29 +0900

    feat:Postcontent.js ongoing

--- client/src/components/PostContent.js
+++ client/src/components/PostContent.js
@@ -1,18 +1,21 @@
 import "../styles/components/PostContent.css";
+import { useNavigate, Link } from "react-router-dom";
 
-const PostContent = ({ content, nickName, modifiedAt }) => {
+const PostContent = ({ content, nickName, modifiedAt, memberId }) => {
+    const navigate = useNavigate();
   return (
     <div className="post-content__container">
       <div className="post-content__content">{content}</div>
       <div className="post-content__meta">
         <div className="post-content__actions">
-          <button>Edit</button>
-          <button>Delete</button>
-          <button>Add a comment</button>
+          <Link to={'#'}>Edit</Link>
+          <p>Delete</p>
+          <p>Add a comment</p>
         </div>
         <div className="post-content__details">
           <p className="post-content__timestamp">{modifiedAt}</p>
-          <h1 className="post-content__author">{nickName}</h1>
+          <h1 onClick={()=>navigate(`/members/${memberId}`)} className="post-content__author">{nickName}</h1>
+          {/* mypage 프론트 라우팅 */}
         </div>
       </div>
     </div>
