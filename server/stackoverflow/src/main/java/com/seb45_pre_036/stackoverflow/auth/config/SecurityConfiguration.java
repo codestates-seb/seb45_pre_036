b8d5b7dee0e025b43b2bbaaac760bc47628ea07d
0KSY <130116643+0KSY@users.noreply.github.com>
2023-08-21 10:30:57 +0900

    feat

--- server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/auth/config/SecurityConfiguration.java
+++ server/stackoverflow/src/main/java/com/seb45_pre_036/stackoverflow/auth/config/SecurityConfiguration.java
@@ -108,7 +108,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
 
