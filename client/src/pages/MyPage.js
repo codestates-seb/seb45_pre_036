4d5ff4fce62d008345f2aea460f1e9d23cbd616b
박지민 <jumpm9239@gmail.com>
2023-08-22 09:14:45 +0900

    주소 변경

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -10,11 +10,106 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Axiosinstance from "../auth/AxiosConfig";
 import { useParams } from 'react-router-dom';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import UsernameBox from '../components/UsernameBox';
+import axios from "axios";
 
-const MyPage = () => {
+
+export const EditForm = ( ) => {
+  const [ userData, setUserData ] = useState({});
+  const [ newNickName, setNewNickName ] = useState("");
+  const [ newTitle, setNewTitle ] = useState("");
+  const [ newContent, setNewContent ] = useState(""); // 텍스트 편집기 구현해야 함
+  const { memberId } = useParams();
+
+  useEffect(() => {
+    async function fetchUserData() {
+        try {
+            const res = await Axiosinstance.patch('/members/' + memberId);
+            setUserData(res.data.data);
+            console.log(res.data.data); // 뭐가 나오는지 확인
+       }
+        catch (err) {
+            console.log('Error getting profile data: ', err);
+        }
+    }
+
+    fetchUserData();
+}, []);
+
+
+// 받아온 profileData 폼에 반영하기
+const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    const updatedData = {
+        nickName: newNickName,
+        title: newTitle,
+        content: newContent
+    };
+
+    try {
+        const res = await Axiosinstance.patch('/members/' + memberId, updatedData);
+        setUserData(res.data.data); 
+    }
+
+    catch (err) {
+            console.log('Error updating profile data: ', err);
+    }
+}
+
+
+return (
+    <>
+    <div>
+    <form>
+        <div>Display Name</div>
+        <input
+            type="text"
+            value={userData.nickName}
+            onChange={e => setNewNickName(e.target.value)}
+        />
+        <br />
+        <div>Title</div>
+        <input
+            type="text"
+            value={userData.title}
+            onChange={e => setNewTitle(e.target.value)}
+        />
+        <div> {/* 텍스트 편집기 */}
+            <CKEditor
+                editor={ ClassicEditor }
+                data={newContent}
+                onReady={ editor => {
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( event, editor ) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                    setNewContent(data);
+                } }
+                onBlur={ ( event, editor ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }
+            />
+        </div>
+        <a href="/members/{member-id}" onClick={handleUpdate}>Submit</a>
+        <button>Cancel</button>
+    </form>
+    </div>
+    </>
+)
+}
+
+
+const MyPage = ( ) => {
   const memberId = useParams();
   const [userData, setUserData] = useState({});
-
+  const [ isEditing, setIsEditing ] = useState(false);
 
   useEffect(() => {
     async function fetchUserData() {
@@ -33,6 +128,8 @@ const MyPage = () => {
 
 
   return (
+    <div>
+    { !isEditing && (
     <div>
       <div className="top__line"></div>
       <img src={require("../static/profile-picture.png")} alt="profile" />
@@ -54,7 +151,7 @@ const MyPage = () => {
       </div>
       <div className="button__box">
         <div className="edit button border">
-          <a href="/members/edit/{member-id}">
+          <a href="/members/edit/{memberId}" >
             <FontAwesomeIcon icon={faPencil} /> Edit Profile
           </a>
         </div>
@@ -80,7 +177,9 @@ const MyPage = () => {
         </div>
       </div>
     </div>
-  );
+    )}
+    </div>
+  )
 };
 
 export default MyPage;
