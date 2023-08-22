e9d57f01f2b3a5eb21f400c84e5274df97bfe9a6
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-22 18:04:58 +0900

    fix: url 변경

--- client/src/auth/AxiosConfig.js
+++ client/src/auth/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axiosinstance = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}`
+    baseURL: `${process.env.BASE_URL}`
 })
 
 export default Axiosinstance;
